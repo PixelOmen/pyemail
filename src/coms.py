6ed696b263a0fc10c3ba3158f6d7556737a93503
Eman <eacosta@roundabout.com>
2023-09-27 12:22:38 -0700

    Memory guard in Connection.idle now attemps to restart idle

--- src/coms.py
+++ src/coms.py
@@ -23,6 +23,11 @@ class Connection:
         return self._connection #type: ignore
 
     def login(self) -> None:
+        if self._connection is not None:
+            try:
+                self.logout()
+            except:
+                pass
         self._connection = imaplib.IMAP4_SSL(self.server)
         self._connection.login(self.user, self.pw)
         self._connection.select(self.mailbox)
@@ -32,41 +37,6 @@ class Connection:
             return
         self._connection.logout()
         self._connection = None
-
-    def idle(self, stopevent: "Event", tag: bytes=b"A001", logger: "Logger"=...) -> list[bytes]:
-        idlecmd = tag + b" IDLE\r\n"
-        self.mail.send(idlecmd)
-        first_response = self.mail.readline()
-        if first_response != b"+ idling\r\n":
-            raise IOError(f"Didn't enter idle: {first_response}")
-        unsolicted = []
-        while not stopevent.is_set():
-            rlist, _, _ = select.select([self.mail.sock], [], [], 1)
-            if not rlist:
-                continue
-            unsolicted = [self.mail.readline()]
-            if unsolicted[-1].startswith(b'* '):
-                self.mail.send(b"DONE\r\n")
-                memoryguard = 0
-                try:
-                    while True:
-                        if unsolicted[-1].startswith(tag):
-                            unsolicted.pop()
-                            break
-                        if memoryguard > 10:
-                            if logger is not ...:
-                                logger.warning("pyemail.Connection.idle - Memoryguard triggered")
-                                logger.debug("Unsolicted messages:")
-                                for msg in unsolicted:
-                                    logger.debug(msg)
-                            raise IOError("Memoryguard triggered")
-                        unsolicted.append(self.mail.readline())
-                        memoryguard += 1
-                except IOError:
-                    unsolicted = []
-                    continue
-                break
-        return unsolicted
 
     def get_ids(self, unread_only: bool=False, read_only: bool=False) -> list[str]:
         if unread_only and read_only:
@@ -91,3 +61,68 @@ class Connection:
         if not data[0]:
             return None
         return message_from_bytes(data[0][1]) #type: ignore
+
+    def idle(self, stopevent: "Event", tag: bytes=b"A001", logger: "Logger"=...) -> list[bytes]:
+        idlecmd = tag + b" IDLE\r\n"
+        self.mail.send(idlecmd)
+        first_response = self.mail.readline()
+        if first_response != b"+ idling\r\n":
+            raise IOError(f"Didn't enter idle: {first_response}")
+        unsolicted = []
+        while not stopevent.is_set():
+            rlist, _, _ = select.select([self.mail.sock], [], [], 1)
+            if not rlist:
+                continue
+            unsolicted = [self.mail.readline()]
+            if unsolicted[-1].startswith(b'* '):
+                self.mail.send(b"DONE\r\n")
+                try:
+                    self._flush_for_done(tag, unsolicted, logger)
+                except IOError:
+                    unsolicted = []
+                    try:
+                        self._restart_idle_memguard(idlecmd)
+                    except IOError as e:
+                        if logger is not ...:
+                            logger.critical(e)
+                        raise e
+                    continue
+                break
+        return unsolicted
+
+    def _flush_for_done(self, tag: bytes, unsolicted: list[bytes], logger: "Logger"=...) -> None:
+        memoryguard = 0
+        while True:
+            if unsolicted[-1].startswith(tag):
+                unsolicted.pop()
+                break
+            if memoryguard > 10:
+                if logger is not ...:
+                    logger.warning("pyemail.Connection.idle - Memoryguard triggered")
+                    logger.debug("Unsolicted messages:")
+                    for msg in unsolicted:
+                        logger.debug(msg)
+                raise IOError("Memoryguard triggered")
+            unsolicted.append(self.mail.readline())
+            memoryguard += 1
+
+    def _restart_idle_memguard(self, idlecmd: bytes) -> None:
+        while True:
+            rlist, _, _ = select.select([self.mail.sock], [], [], 5)
+            if not rlist:
+                raise IOError("Could not restart Connection.Idle - Timeout - No response from DONE on memoryguard")
+            msg = self.mail.readline()
+            if msg.startswith(b'* '):
+                attempts = 0
+                rlist, _, _ = select.select([self.mail.sock], [], [], 5)
+                while rlist is not None:
+                    if attempts > 10:
+                        raise IOError("Could not restart Connection.Idle - Too many messages after DONE on memoryguard")
+                    self.mail.readline()
+                    rlist, _, _ = select.select([self.mail.sock], [], [], 1)
+                    attempts += 1
+                self.mail.send(idlecmd)
+                first_response = self.mail.readline()
+                if first_response != b"+ idling\r\n":
+                    raise IOError(f"Unable to restart idle after memoryguard - idle response: {first_response}")
+                break
